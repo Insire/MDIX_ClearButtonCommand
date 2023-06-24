25a406f3d945a35eaf430f6cd2b7ecbc0d334bb0
Peter Vietense <peter.vietense@hotmail.de>
2023-06-24 07:27:46 +0200

    wip hookup smarthint clearcommand to MultiSelectionViewModel.DeSelectAllCommand

--- MDIX_ClearButtonCommand/MultiSelectComboBox.cs
+++ MDIX_ClearButtonCommand/MultiSelectComboBox.cs
@@ -1,10 +1,25 @@
-﻿using System.Windows;
+﻿using MaterialDesignThemes.Wpf;
+using MaterialDesignThemes.Wpf.Internal;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MDIX_ClearButtonCommand
 {
+
     public sealed class MultiSelectComboBox : ComboBox
     {
+        static MultiSelectComboBox()
+        {
+            ClearText.HandlesClearCommandProperty.OverrideMetadata(typeof(MultiSelectComboBox), new FrameworkPropertyMetadata(OnHandlesClearCommandChanged));
+            HintProxyFabric.RegisterBuilder(c => c is MultiSelectComboBox, c => new MultiSelectComboBoxHintProxy((MultiSelectComboBox)c));
+
+            var buildersInfo = typeof(HintProxyFabric).GetField("Builders", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            var buildersInstance = buildersInfo.GetValue(null);
+            // HintProxyBuilder is a private type, so its kinda hard to build this
+            // var builders = buildersInstance as List<HintProxyBuilder> 
+        }
+
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new ContentControl();
@@ -13,6 +28,42 @@ namespace MDIX_ClearButtonCommand
         protected override bool IsItemItsOwnContainerOverride(object item)
         {
             return item is UIElement;
+        }
+
+        private static void OnHandlesClearCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MultiSelectComboBox element)
+            {
+                if ((bool)e.NewValue)
+                {
+                    RemoveClearTextCommand(element);
+                    element.CommandBindings.Add(new CommandBinding(ClearText.ClearCommand, OnClearCommand));
+                }
+                else
+                {
+                    RemoveClearTextCommand(element);
+                }
+            }
+
+            static void RemoveClearTextCommand(MultiSelectComboBox element)
+            {
+                for (int i = element.CommandBindings.Count - 1; i >= 0; i--)
+                {
+                    if (element.CommandBindings[i].Command == ClearText.ClearCommand)
+                    {
+                        element.CommandBindings.RemoveAt(i);
+                    }
+                }
+            }
+
+            static void OnClearCommand(object sender, ExecutedRoutedEventArgs e)
+            {
+                if (sender is MultiSelectComboBox element && element.DataContext is MultiSelectionViewModel viewModel)
+                {
+                    viewModel.DeSelectAllCommand.Execute(null);
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
---
+++ MDIX_ClearButtonCommand/MultiSelectComboBoxHintProxy.cs
@@ -0,0 +1,56 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MDIX_ClearButtonCommand
+{
+    // wip
+    public sealed class MultiSelectComboBoxHintProxy : IHintProxy
+    {
+        private readonly MultiSelectComboBox _multiSelectComboBox;
+
+        public bool IsLoaded => _multiSelectComboBox.IsLoaded;
+
+        public bool IsVisible => _multiSelectComboBox.IsVisible;
+
+        public event EventHandler? ContentChanged;
+        public event EventHandler? IsVisibleChanged;
+        public event EventHandler? Loaded;
+        public event EventHandler? FocusedChanged;
+
+        public MultiSelectComboBoxHintProxy(MultiSelectComboBox multiSelectComboBox)
+        {
+            _multiSelectComboBox = multiSelectComboBox;
+            _multiSelectComboBox.Loaded += ComboBoxLoaded;
+            _multiSelectComboBox.IsVisibleChanged += ComboBoxIsVisibleChanged;
+            _multiSelectComboBox.IsKeyboardFocusWithinChanged += ComboBoxIsKeyboardFocusWithinChanged;
+        }
+        private void ComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+            => _multiSelectComboBox.Dispatcher.InvokeAsync(() => ContentChanged?.Invoke(sender, EventArgs.Empty));
+        private void ComboBoxIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+            => IsVisibleChanged?.Invoke(sender, EventArgs.Empty);
+        private void ComboBoxLoaded(object sender, RoutedEventArgs e)
+            => Loaded?.Invoke(sender, EventArgs.Empty);
+
+        private void ComboBoxIsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+            => FocusedChanged?.Invoke(sender, EventArgs.Empty);
+
+        public void Dispose()
+        {
+            _multiSelectComboBox.Loaded -= ComboBoxLoaded;
+            _multiSelectComboBox.IsVisibleChanged -= ComboBoxIsVisibleChanged;
+            _multiSelectComboBox.IsKeyboardFocusWithinChanged -= ComboBoxIsKeyboardFocusWithinChanged;
+        }
+
+        public bool IsEmpty()
+        {
+            return false;
+        }
+
+        public bool IsFocused()
+        {
+            return false;
+        }
+    }
+}
